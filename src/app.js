e4d57331ecfe3095cd1acc09aa39ff368aec3dc5
Fajar Maulana Firdaus <Fajar.Firdaus@btpn.com>
2021-10-23 09:24:19 +0700

    [Fajar] Inject Organization to application

--- src/app.js
+++ src/app.js
@@ -3,11 +3,27 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import database from './Database';
 import config from '../config/index';
+import Organization from "./organization/Organization";
+import OrganizationService from "./organization/OrganizationService";
 
 const app = express();
 app.use(cors());
 const databaseConnection = database.connect(config.db);
 
+const createModels = () => ({
+    Organization: Organization.init(databaseConnection)
+});
+
+const createServices = models => ({
+    organizationService: new OrganizationService(models)
+});
+
+const registerDependencies = () => {
+    app.locals.models = createModels();
+    app.locals.services = createServices(app.locals.models);
+};
+
+registerDependencies();
 app.use(bodyParser.json());
 
 export default app;
