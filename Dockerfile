d5730fe34038d46c2eae701696f9dbff7a87bea5
Fajar Maulana Firdaus <Fajar.Firdaus@btpn.com>
2021-10-25 12:50:20 +0700

    MAC SHIT

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,11 @@ WORKDIR /usr/src/app
 COPY . /usr/src/app
 RUN npm install
 
+ENV NODE_ENV=dev
+#ENV WAIT_VERSION 2.7.2
+#
+#ADD https://github.com/ufoscout/docker-compose-wait/releases/download/$WAIT_VERSION/wait /wait
+#RUN chmod +x /wait
+#
+#CMD ["/sayhello"]
 CMD [ "npm", "start" ]
--- seeds/fill-tables.sql
+++ seeds/fill-tables.sql
@@ -1,35 +1,3 @@
-CREATE TABLE "public"."comment" (
-                                    "id" varchar(50) NOT NULL,
-                                    "comment" text NOT NULL,
-                                    "organization_id" varchar(50) NOT NULL,
-                                    "is_deleted" BOOL NOT NULL ,
-                                    "created_at" timestamp,
-                                    "updated_at" timestamp,
-                                    PRIMARY KEY ("id")
-);
-
-CREATE TABLE "public"."organization" (
-                                         "id" varchar,
-                                         "organization_name" varchar(255),
-                                         "created_at" timestamp,
-                                         "updated_at" timestamp,
-                                         PRIMARY KEY ("id")
-);
-
-CREATE TABLE "public"."member" (
-                             "id" varchar,
-                             "login" varchar(255) NOT NULL,
-                             "avatar_url" varchar(255) NOT NULL,
-                             "followers_url" varchar(255) NOT NULL,
-                             "following_url" varchar(255) NOT NULL,
-                             "organization_id" varchar(50) NOT NULL,
-                             "followers" bigint NOT NULL,
-                             "following" bigint NOT NULL,
-                             "created_at" timestamp,
-                             "updated_at" timestamp,
-                             PRIMARY KEY ("id")
-);
-
 INSERT INTO public.organization (id,organization_name,created_at,updated_at)
 VALUES ('xendit','Xendit','2021-10-24 17:47:54.942','2021-10-24 17:47:54.942');
 INSERT INTO public.organization (id,organization_name,created_at,updated_at)
